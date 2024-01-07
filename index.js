67f941822b146b3a13fb9b338426e07525a8670c
Mikołaj Mrózek <31847808+reqill@users.noreply.github.com>
2024-01-07 14:30:32 +0100

    fix: vercel deployment

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-import express from "express";
-import { getParameters } from "codesandbox/lib/api/define";
-import helmet from "helmet";
-import cors from "cors";
-import rateLimit from "express-rate-limit";
-import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
+const express = require("express");
+const { getParameters } = require("codesandbox/lib/api/define");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
-const app = express(),
-  PORT = 8080,
-  MAX_FILES = 42,
-  CODE_SANDBOX_API = "https://codesandbox.io/api/v1/sandboxes/define",
-  CODE_SANBOX_BY_ID = "https://codesandbox.io/s";
-
-if (!process.env.VERCEL) {
-  app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const app = express();
+const PORT = process.env.PORT || 8080;
+const MAX_FILES = 42;
+const CODE_SANDBOX_API = "https://codesandbox.io/api/v1/sandboxes/define";
+const CODE_SANBOX_BY_ID = "https://codesandbox.io/s";
 
 app.use(
   helmet.contentSecurityPolicy({
@@ -36,29 +30,17 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const swaggerDocument = YAML.load(__dirname + "/openapi.yaml");
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-type GeneralPostPayload = {
-  name: string;
-  dependencies: { [key: string]: string };
-  files: {
-    [key: string]: {
-      content: string;
-      isBinary?: boolean;
-    };
-  };
-};
-
 app.get("/", (req, res) => {
   res.send("This API handles POST requests only");
 });
 
-app.post<{}, {}, GeneralPostPayload>("/", async (req, res) => {
+app.post("/", async (req, res) => {
   const { name, dependencies, files } = req.body;
 
   if (Object.keys(files).length === 0) {
@@ -81,7 +63,7 @@ app.post<{}, {}, GeneralPostPayload>("/", async (req, res) => {
     return;
   }
 
-  const filesForSandbox: any = {
+  const filesForSandbox = {
     "package.json": { content: { name, dependencies } },
     ...files,
   };
@@ -96,12 +78,16 @@ app.post<{}, {}, GeneralPostPayload>("/", async (req, res) => {
     body: form,
   });
 
-  const { sandbox_id } = (await sandboxResponse.json()) as {
-    sandbox_id: string;
-  };
+  const { sandbox_id } = await sandboxResponse.json();
   const codeSandboxUrl = `${CODE_SANBOX_BY_ID}/${sandbox_id}`;
 
   res.status(200).send(codeSandboxUrl);
 });
 
-export default app;
+if (process.env.DEV === "true" && !process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
